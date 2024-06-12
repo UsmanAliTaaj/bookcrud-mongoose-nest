f972f04fcfe11dfa5847979ab97a6fa4bd0be1f0
UsmanAliTaaj <usman.ali.taaj@gmail.com>
2024-06-12 16:49:19 +0500

    update dto

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,11 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
-import { Category } from '../schemas/book.schema';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {
-    readonly title: string;
-  readonly description: string;
-  readonly author: string;
-  readonly price: number;
-  readonly category: Category;
-}
+export class UpdateBookDto extends PartialType(CreateBookDto) {}
